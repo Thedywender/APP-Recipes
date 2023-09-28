cbcb31cc3853e682adc47274f7bc88f3e7936fd5
Thedy Wender <thedywender@hotmail.com>
2023-09-28 16:52:38 -0400

    req37ao43 grupo-16

--- src/components/IngredientsIput/IngredientsInput.tsx
+++ src/components/IngredientsIput/IngredientsInput.tsx
@@ -1,52 +1,69 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 type IngredientsProps = {
   ingredient: string;
   indexFilter: number;
   id: string;
+  setCheckedbtn: (state: boolean) => void;
+  ingredientLength: number;
 };
 
-function IngredientsInput({ ingredient, indexFilter, id }: IngredientsProps) {
+function IngredientsInput({ ingredient,
+  indexFilter, id, setCheckedbtn, ingredientLength }: IngredientsProps) {
   const location = useLocation();
   const currentPath = location.pathname.includes('/meals') ? 'meals' : 'drinks';
   const [checking, setChecking] = useState(false);
-  // console.log(ingredient);
-  //
+
   const handleChecked = ({
     target: { checked } }: ChangeEvent<HTMLInputElement>) => {
-    const getStorage = localStorage.getItem('inProgressRecipes')
+    if (checked) {
+      const getStorage = localStorage.getItem('inProgressRecipes')
     || JSON.stringify({ [currentPath]: { [id]: [] } });
 
-    if (JSON.parse(getStorage[currentPath])) {
-      const storageParsed = JSON.parse(getStorage);
-      const ingredientsArray = storageParsed[currentPath][id];
-      console.log(ingredientsArray);
-      const newObject = {
-        ...storageParsed,
-        [currentPath]: { [id]: [...ingredientsArray, ingredient] },
-      };
-      localStorage.setItem('inProgressRecipes', JSON.stringify(newObject));
-      // storageParsed[currentPath][id](ingredient);
-      // const ExistId = storageParsed[currentPath][id];
-      // if (ExistId) {
-    //     storageParsed[currentPath][id] = !checked ? [...ExistId, ingredient]
-    //       : ExistId.filter((item: string) => item !== ingredient);
-    //   }
+      if (JSON.parse(getStorage)[currentPath]) {
+        const storageParsed = JSON.parse(getStorage);
+        const ingredientsArray = storageParsed[currentPath][id];
+        console.log(ingredientsArray);
+        const newObject = {
+          ...storageParsed,
+          [currentPath]: { [id]: [...ingredientsArray, ingredient] },
+        };
+        const verifyFinishBtn = newObject[currentPath][id].length === ingredientLength;
+        setCheckedbtn(!verifyFinishBtn);
+        localStorage.setItem('inProgressRecipes', JSON.stringify(newObject));
+      } else {
+        const newObject = {
+          ...JSON.parse(getStorage),
+          [currentPath]: { [id]: [ingredient] },
+        };
+        const verifyFinishBtn = newObject[currentPath][id].length === ingredientLength;
+        setCheckedbtn(!verifyFinishBtn);
+        localStorage.setItem('inProgressRecipes', JSON.stringify(newObject));
+      }
     } else {
-      const inProgressRecipes: any = {
-        meals: {},
-        drinks: {},
-      };
-      localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipes));
+      setChecking(false);
+      const getStorage = JSON.parse(localStorage.getItem('inProgressRecipes') as string);
+      const newArray = getStorage[currentPath][id]
+        .filter((item: string) => item !== ingredient);
+      localStorage.setItem('inProgressRecipes', JSON.stringify({
+        ...getStorage,
+        [currentPath]: { [id]: newArray },
+      }));
+      setCheckedbtn(true);
     }
-    setChecking(!checked);
+    setChecking(checked);
   };
+
+  useEffect(() => {
+    const storage = JSON.parse(localStorage.getItem('inProgressRecipes') || '{}');
+    setChecking(storage[currentPath]?.[id]?.includes(ingredient) || false);
+  }, [currentPath, id, ingredient]);
 
   return (
     <label
-      className={ checking ? 'checkboxFiltered' : 'inputCheckbox' }
       data-testid={ `${indexFilter}-ingredient-step` }
+      className={ checking ? 'checkboxFiltered' : 'inputCheckbox' }
     >
       <input
         type="checkbox"
--- src/components/RecipeInProgress/index.tsx
+++ src/components/RecipeInProgress/index.tsx
@@ -1,18 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import blackHeartIcon from '../../images/blackHeartIcon.svg';
+import shareIcon from '../../images/shareIcon.svg';
+import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
 import './recipesInProgress.css';
 
 import { NewMealsType } from '../../types';
 import { apiDetailsByIdDrinks, apiDetailsByIdMeals } from '../../utils/apirecipesById';
 import IngredientsInput from '../IngredientsIput/IngredientsInput';
+import recipeContext from '../../context/recipeContext';
 
 function RecipesInProgress() {
+  const {
+    toggleFavorite,
+    shareValue,
+    share,
+  } = useContext(recipeContext);
   const { id } = useParams();
   const location = useLocation();
   const currentPath = location.pathname;
 
+  const navigate = useNavigate();
+
+  const sharImg = (<img
+    src={ shareIcon }
+    alt={ shareIcon }
+  />);
+
   const [recipesInProgress, setRecipesInProgress] = useState<NewMealsType[]>([]);
-  const [filterRecipes, setFilterRecipes] = useState<NewMealsType[]>([]);
+  const [filterRecipes, setFilterRecipes] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [checkedbtn, setCheckedbtn] = useState(true);
 
   const [mealOrDrink, setMealorDrink] = useState('');
   const includMeals = currentPath.includes('/meals');
@@ -20,7 +38,7 @@ function RecipesInProgress() {
   useEffect(() => {
     if (currentPath.includes('/meals')) {
       const apiMealsRecipes = async () => {
-        const data = await apiDetailsByIdMeals(id);
+        const data = await apiDetailsByIdMeals(id as string);
         setRecipesInProgress(data.meals);
         setMealorDrink('Meal');
         filterIngredients(data.meals);
@@ -28,12 +46,19 @@ function RecipesInProgress() {
       apiMealsRecipes();
     } else {
       const apiDrinksRecipes = async () => {
-        const data = await apiDetailsByIdDrinks(id);
+        const data = await apiDetailsByIdDrinks(id as string);
         setRecipesInProgress(data.drinks);
         setMealorDrink('Drink');
         filterIngredients(data.drinks);
       };
       apiDrinksRecipes();
+    }
+    const storage = localStorage.getItem('favoriteRecipes');
+    if (storage) {
+      const favoriteRecipes = JSON.parse(storage);
+      const isFavoriteRecipe = favoriteRecipes
+        .some((recipe: any) => recipe.id === id);
+      setIsFavorite(isFavoriteRecipe);
     }
   }, [id]);
 
@@ -45,9 +70,42 @@ function RecipesInProgress() {
     setFilterRecipes(result);
   };
 
+  const createRecipesDone = (item: any) => {
+    return {
+      id: item[`id${currentPath.includes('/meals') ? 'Meal' : 'Drink'}`],
+      type: currentPath.includes('/meals') ? 'meal' : 'drink',
+      nationality: item.strArea || '',
+      category: item.strCategory || '',
+      alcoholicOrNot: item.strAlcoholic || '',
+      name: item[`str${currentPath.includes('/meals') ? 'Meal' : 'Drink'}`],
+      image: item[`str${currentPath.includes('/meals') ? 'Meal' : 'Drink'}Thumb`],
+      doneDate: new Date(),
+      tags: item?.strTags?.split(',') || [],
+    };
+  };
+  function toggleDoneRecipes(item: any) {
+    const favoriteItem = createRecipesDone(item);
+    const getStorage = localStorage.getItem('doneRecipes');
+    if (getStorage) {
+      const storageParsed = JSON.parse(getStorage);
+      const doneRecipes = storageParsed
+        .filter((done: any) => done.id !== favoriteItem.id);
+      if (doneRecipes.length === storageParsed.length) {
+        localStorage.setItem(
+          'doneRecipes',
+          JSON.stringify([...storageParsed, favoriteItem]),
+        );
+      } else {
+        localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+      }
+    } else {
+      localStorage.setItem('doneRecipes', JSON.stringify([favoriteItem]));
+    }
+  }
+
   return (
     <div>
-      {recipesInProgress.map((recipe: any, index) => (
+      {recipesInProgress.map((recipe: any) => (
         <div key={ recipe[`id${mealOrDrink}`] }>
           <div>
             <img
@@ -60,28 +118,57 @@ function RecipesInProgress() {
             <h2 data-testid="recipe-title">{recipe[`str${mealOrDrink}`]}</h2>
             <h2 data-testid="recipe-category">
               {
-          includMeals ? recipe.strCategory : recipe.strAlcoholic
+                includMeals ? recipe.strCategory : recipe.strAlcoholic
 }
             </h2>
-            <button data-testid="share-btn">compartilhar</button>
-            <button data-testid="favorite-btn">favoritar</button>
+            <button
+              data-testid="share-btn"
+              onClick={ () => shareValue() }
+            >
+              { sharImg }
+
+            </button>
+            <p>{ share && 'Link copied!'}</p>
+            <button
+              onClick={ () => { toggleFavorite(recipe); setIsFavorite(!isFavorite); } }
+            >
+              <img
+                data-testid="favorite-btn"
+                src={ !isFavorite ? whiteHeartIcon : blackHeartIcon }
+                alt={ !isFavorite ? whiteHeartIcon : blackHeartIcon }
+              />
+
+            </button>
             <h2
               data-testid="instructions"
               style={
-            { fontSize: 12 }
-}
+                { fontSize: 12 }
+              }
             >
               {recipe.strInstructions}
 
             </h2>
-            <button data-testid="finish-recipe-btn">finalizar</button>
+            <button
+              onClick={ () => {
+                toggleDoneRecipes(recipe);
+                navigate('/done-recipes');
+              } }
+              data-testid="finish-recipe-btn"
+              disabled={ checkedbtn }
+            >
+              Finish Recipe
+
+            </button>
           </div>
           <div>
             {filterRecipes.map((ingredient, indexFilter) => (
               <IngredientsInput
                 key={ indexFilter }
                 ingredient={ ingredient }
-                id={ id }
+                id={ id as string }
+                indexFilter={ indexFilter }
+                setCheckedbtn={ setCheckedbtn }
+                ingredientLength={ filterRecipes.length }
               />
             ))}
           </div>
