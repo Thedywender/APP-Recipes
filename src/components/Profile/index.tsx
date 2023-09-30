bdc614e712fa60d4f3436e9588e294788b78a386
Gabriel Mouro <gabrielmouro38@gmail.com>
2023-09-30 11:44:05 -0300

    requisitos feitos sem os testes ainda

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,9 +1,47 @@
+import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 
 export default function Profile() {
+  const localStorageData = localStorage.getItem('user');
+  const parsedData = JSON.parse(localStorageData);
+  const navigate = useNavigate();
+  const logOut = () => {
+    localStorage.clear();
+    navigate('/');
+  }
+
   return (
     <>
-      <div>oi</div>
+      <h3
+        data-testid="profile-email"
+      >
+        { parsedData.email }
+      </h3>
+      <br />
+      <br />
+      
+      <button
+        data-testid="profile-done-btn"
+        onClick={ () => navigate('/done-recipes') }
+      >
+          Done Recipes
+      </button>
+      <br />
+      <br />
+      <button
+        data-testid="profile-favorite-btn"
+        onClick={ () => navigate('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <br />
+      <br />
+      <button
+        data-testid="profile-logout-btn"
+        onClick={ logOut }
+      >
+        Logout
+      </button>
       <Footer />
     </>
   );
