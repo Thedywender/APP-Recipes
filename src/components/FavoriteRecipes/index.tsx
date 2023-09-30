be1476f34312970713af72af29fea75a1b204423
Thedy Wender <thedywender@hotmail.com>
2023-09-30 11:34:01 -0400

    reqaté a 53

--- src/components/FavoriteRecipes/index.tsx
+++ src/components/FavoriteRecipes/index.tsx
@@ -1,5 +1,124 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import mealIcon from '../../images/mealIcon.svg';
+import drinkIcon from '../../images/drinkIcon.svg';
+import shareImg from '../../images/shareIcon.svg';
+import recipeContext from '../../context/recipeContext';
+import blackHeartIcon from '../../images/blackHeartIcon.svg';
+
 export default function FavoriteRecipes() {
+  const { share, setShare, toggleFavorite } = useContext(recipeContext);
+
+  const [mealsDoneRecipes, setMealsDoneRecipes] = useState([]);
+  const [drinksDoneRecipes, setDrinksDoneRecipes] = useState([]);
+  const [FilteredRecipes, setFilteredRecipes] = useState([]);
+
+  useEffect(() => {
+    const getLocalStorage = localStorage.getItem('favoriteRecipes');
+    const parsedLocalStorage = JSON.parse(getLocalStorage);
+
+    if (parsedLocalStorage) {
+      const mealRecipes = parsedLocalStorage.filter(
+        (recipe) => recipe.type === 'meal',
+      );
+
+      const drinkRecipes = parsedLocalStorage.filter(
+        (recipe) => recipe.type === 'drink',
+      );
+
+      setMealsDoneRecipes(mealRecipes);
+      setDrinksDoneRecipes(drinkRecipes);
+      setFilteredRecipes(parsedLocalStorage);
+      console.log(parsedLocalStorage);
+    }
+  }, []);
+
+  const handleFilterClick = (filter) => {
+    let filteredRecipes = [];
+
+    if (filter === 'meals') {
+      filteredRecipes = mealsDoneRecipes;
+    } else if (filter === 'drinks') {
+      filteredRecipes = drinksDoneRecipes;
+    } else {
+      filteredRecipes = [...mealsDoneRecipes, ...drinksDoneRecipes];
+    }
+
+    setFilteredRecipes(filteredRecipes);
+  };
+
+  const shareValueLink = (linked: string) => {
+    const link = `http://localhost:3000${linked}`;
+    navigator.clipboard.writeText(link);
+    setShare(!share);
+    return link;
+  };
+
   return (
-    <div>oi</div>
+    <div>
+      <button
+        data-testid="filter-by-all-btn"
+        onClick={ () => handleFilterClick('all') }
+      >
+        All
+      </button>
+      <button
+        data-testid="filter-by-meal-btn"
+        onClick={ () => handleFilterClick('meals') }
+      >
+        <img src={ mealIcon } alt="" />
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        onClick={ () => handleFilterClick('drinks') }
+      >
+        <img src={ drinkIcon } alt="" />
+
+      </button>
+
+      {FilteredRecipes.map((recipe, index) => (
+        <div key={ index }>
+          <img
+            data-testid={ `${index}-horizontal-image` }
+            src={ recipe.image }
+            alt={ recipe.name }
+            width={ 200 }
+            height={ 200 }
+          />
+          (recipe.type === 'drinks') ? (
+          <p data-testid={ `${index}-horizontal-top-text` }>
+            {`${recipe.alcoholicOrNot}`}
+          </p>
+          ) : (
+          <p data-testid={ `${index}-horizontal-top-text` }>
+            {`${recipe.nationality} - ${recipe.category}`}
+          </p>
+          )
+          <Link to={ `/${recipe.type}s/${recipe.id}` }>
+            <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+          </Link>
+          <button
+            onClick={ () => shareValueLink(`/${recipe.type}s/${recipe.id}`) }
+          >
+            <img
+              data-testid={ `${index}-horizontal-share-btn` }
+              src={ shareImg }
+              alt=""
+            />
+          </button>
+          <button
+            onClick={ () => toggleFavorite(recipe) }
+          >
+            <img
+              data-testid={ `${index}-horizontal-favorite-btn` }
+              src={ blackHeartIcon }
+              alt={ blackHeartIcon }
+            />
+
+          </button>
+        </div>
+      ))}
+      <p>{ share && 'Link copied!'}</p>
+    </div>
   );
 }
